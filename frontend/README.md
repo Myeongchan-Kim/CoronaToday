16309fa71c944baca4721c5254da79d5e749262f
Myeongchan-Kim <myeongchan.kim@nulogix.com>
2020-05-16 20:34:45 -0400

    succress run

---
+++ frontend/README.md
@@ -0,0 +1,9 @@
+# CoronaToday
+A Toy project of Boston Korean NLP study club. To share the current COVID-19 status and information for Korean-American.
+
+# 기본 컨셉
+https://docs.google.com/presentation/d/1s8OVlnY8HvZXDZEuuRJdNwjXPZ1Dgg5UhUh8ws6NRoY/edit#slide=id.p  
+(계속 업데이트 중입니다.)
+
+# 마일스톤 및 실행 방법
+https://github.com/Myeongchan-Kim/CoronaToday/wiki
--- frontend/covid_case_data_maker.py
+++ frontend/covid_case_data_maker.py
@@ -1,8 +1,27 @@
 
 import os, sys
+import pandas as pd
 import subprocess, shutil
 
+
+def data_pull():
+    os.chdir("../COVID-19")
+    os.system("git pull origin master")
+    os.chdir("../frontend")
+
+def convert_data_file(src_filepath, dst_filepath):
+    df = pd.read_csv(src_filepath, dtype=str)
+    df = df.rename(columns={"Lat": "Lat_", "Long_": "Long_"})
+    df.to_csv(dst_filepath, index=None)
+    print("Successfully saved a file : {}".format(dst_filepath))
+
 if __name__ == "__main__":
-    src_filepath = "../COVID-19/csse_covid_19_data/csse_covid_19_time_series.csv"
-    dst_filepath = "data/csse_covid_19_time_series.csv"
-    subprocess.run('cp', src_filepath, dst_filepath)
+
+    # Update the latest data
+    data_pull()
+
+    src_filepath = "../COVID-19/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_US.csv"
+    dst_filepath = "../frontend/data/time_series_covid19_confirmed_US.csv"
+
+    convert_data_file(src_filepath, dst_filepath)
+
